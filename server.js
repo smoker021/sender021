fc2993876e80588990238618e915c10fe3681d34
smoker021 <erfan021turk@gmail.com>
2025-01-24 09:18:10 -0900

    Fixed ESLint issues

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const nodemailer = require("nodemailer");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(
   cors({
